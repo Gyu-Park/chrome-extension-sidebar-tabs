0454d3ffe674a42d842acc40e30310dc7259ae54
Gyu-Park <kyesung8282@gmail.com>
2023-01-08 21:31:13 -0500

    added functionality that can manipulate tabs using elements on sidebar

--- sidebar.js
+++ sidebar.js
@@ -11,23 +11,23 @@ chrome.runtime.onMessage.addListener(function(msg, sender){
 var iframe = document.createElement('iframe'); 
 iframe.style.background = "white";
 iframe.style.height = "100%";
-iframe.style.width = "0px";
+iframe.style.width = "400px";
 iframe.style.position = "fixed";
 iframe.style.top = "0px";
-iframe.style.right = "0px";
+iframe.style.right = "-410px";
 iframe.style.zIndex = "9000000000000000000";
 iframe.style.border = "1px solid black";
-iframe.style.transition = "0.5s";
+iframe.style.transition = "0.7s";
 iframe.src = chrome.runtime.getURL("sidebar.html")
 
 document.body.appendChild(iframe);
 
 function toggle(){
-    if(iframe.style.width == "0px"){
-        iframe.style.width="400px";
+    if(iframe.style.right == "-410px"){
+        iframe.style.right="0px";
     }
     else{
-        iframe.style.width="0px";
+        iframe.style.right="-410px";
     }
 }
 
@@ -36,15 +36,23 @@ function query() {
         document.write(`<h3>The tabs you're on are:</h3>`);
         document.write(`<div class="container" style="background-color: white; padding: 0.1rem; margin-top: 1rem">`);
         for (let i = 0; i < tabs.length; i++) {
-            document.write(`<p class="draggable" draggable="true" style="padding: 0.5rem; background-color: white; border: 1px solid black; cursor: move;">
+            document.write(`<p class="draggable" id="${tabs[i].id}" draggable="true" style="padding: 0.5rem; background-color: white; border: 1px solid black; cursor: grab;">
             ${tabs[i].title}</p>`);
         }
         document.write(`</div>`);
     
         const draggables = document.querySelectorAll('.draggable');
         const containers = document.querySelectorAll('.container');
+        let draggableIndex;
+        let afterElementIndex;
     
-        draggables.forEach(draggable => {
+        draggables.forEach((draggable) => {
+            draggable.addEventListener('click', () => {
+                chrome.tabs.update(parseInt(draggable.getAttribute('id')), {active: true});
+                document.body.innerHTML = '';
+                query();
+            });
+
             draggable.addEventListener('dragstart', () => {
                 draggable.classList.add('dragging');
                 draggable.style.opacity = '.5';
@@ -53,18 +61,32 @@ function query() {
             draggable.addEventListener('dragend', () => {
                 draggable.classList.remove('dragging');
                 draggable.style.opacity = '';
+                if (afterElementIndex === tabs.length - 1)
+                    chrome.tabs.move(parseInt(draggable.getAttribute('id')), {index: -1});
+                else if (draggableIndex > afterElementIndex)
+                    chrome.tabs.move(parseInt(draggable.getAttribute('id')), {index: afterElementIndex});
+                else
+                    chrome.tabs.move(parseInt(draggable.getAttribute('id')), {index: afterElementIndex-1});
             });
         });
     
         containers.forEach(container => {
             container.addEventListener('dragover', e => {
+                e.dataTransfer.dropEffect = "move";
                 e.preventDefault();
                 const afterElement = getDragAfterElement(container, e.clientY);
                 const draggable = document.querySelector('.dragging');
                 if (afterElement == null) {
                     container.appendChild(draggable);
+                    // chrome.tabs.move(parseInt(draggable.getAttribute('id')), {index: -1});
                 } else {
                     container.insertBefore(draggable, afterElement);
+                    for (let i = 0; i < tabs.length; i++) {
+                        if (tabs[i].id === parseInt(afterElement.getAttribute('id')))
+                            afterElementIndex = i;
+                        else if (tabs[i].id === parseInt(draggable.getAttribute('id')))
+                            draggableIndex = i;
+                    }
                 }
             })
         });
@@ -88,6 +110,16 @@ function query() {
 query();
 
 chrome.tabs.onActivated.addListener(function() {
+    document.body.innerHTML = '';
+    query();
+});
+
+chrome.tabs.onMoved.addListener(function() {
+    document.body.innerHTML = '';
+    query();
+});
+
+chrome.tabs.onRemoved.addListener(function() {
     document.body.innerHTML = '';
     query();
 });
